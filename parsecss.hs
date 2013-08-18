223ea89d0954c5f8e687b9abdae232994c630b81
Uri Baghin <skerfat@gmail.com>
2013-08-18 13:30:02 +0200

    Switch to Text for UTF8 support.

--- parsecss.hs
+++ parsecss.hs
@@ -2,8 +2,11 @@
 
 module Main where
 
-import Control.Applicative
-import Text.ParserCombinators.Parsec hiding (many, optional, (<|>))
+import qualified Data.Text as T
+import Data.Text (Text)
+import Text.Parsec
+import Text.Parsec.Text
+import Control.Applicative (pure, (<*>), (<*), (*>), (<$>))
 
 type CssComment = String
 
@@ -78,60 +81,74 @@ data CssExpressionTerm = Plus
                        | Identifier CssIdentifier
                        deriving Show
 
-data CssString = Quote String deriving Show
+data CssString = Quote Text deriving Show
 
-type CssIdentifier = String
-type CssName = String
+type CssIdentifier = Text
+type CssName = Text
 
 -- SEE http://www.w3.org/TR/css3-selectors/#lex
 -- SEE http://hackage.haskell.org/package/parsec
 -- SEE http://book.realworldhaskell.org/read/using-parsec.html
 -- TODO return datatype instead of strings for all of this
 
-cssSelectorGroup = (++) <$> cssSelector <*> (try group <|> pure "")
-  where group = (:) <$> char ',' <* whiteSpace <*> cssSelectorGroup
-cssSelector = (++) <$> simpleSelectorSequence <*> (try combined <|> pure "") <* whiteSpace
-  where combined = (:) <$> combinator <*> cssSelector
-        combinator = (try (whiteSpace *> (char '+' <|> char '>' <|> char '~')) <|> space) <* whiteSpace
-        simpleSelectorSequence = try ((++) <$> (try cssTypeSelector <|> cssUniversal) <*> (try selSequence <|> pure "")) <|> selSequence -- todo selSequence should be array
-        selSequence = cssHash <|> cssClass <|> cssAttrib <|> cssPseudo <|> cssNegation
-cssTypeSelector = cssNamespacePrefixed cssElementName
-cssNamespacePrefixed p = try ((++) <$> cssNamespacePrefix <*> p) <|> p
-  where cssNamespacePrefix = (++) <$> (try cssIdentifier <|> string "*" <|> pure "") <*> string "|"
-cssElementName = cssIdentifier
-cssUniversal = cssNamespacePrefixed (string "*")
-cssClass = (:) <$> char '.' <*> cssIdentifier
-cssAttrib = between (char '[') (char ']') ((++) <$> (whiteSpace *> cssNamespacePrefixed cssIdentifier <* whiteSpace) <*> cssAttribValue)
-  where cssAttribValue = try $ option "" ((++) <$> cssOpt <*> (cssIdentifier <|> cssString))
-        cssOpt =  try (string "~=")
-              <|> try (string "|=")
-              <|> try (string "^=")
-              <|> try (string "$=")
-              <|> try (string "*=")
-              <|> string "="
-cssPseudo = (++) <$> pseudoColumn <*> (try functionalPseudo <|> cssIdentifier)
-  where pseudoColumn = try (string "::") <|> string ":"
-        functionalPseudo = (++) <$> (cssIdentifier <* whiteSpace) <*> between (char '(') (char ')') functionParams
-        functionParams = (++) <$> cssExpression <*> (try (many1 space *> functionParams) <|> pure "")
+--cssSelectorGroup = (++) <$> cssSelector <*> (try group <|> pure "")
+--  where group = (:) <$> char ',' <* whiteSpace <*> cssSelectorGroup
+--cssSelector = (++) <$> simpleSelectorSequence <*> (try combined <|> pure "") <* whiteSpace
+--  where combined = (:) <$> combinator <*> cssSelector
+--        combinator = (try (whiteSpace *> (char '+' <|> char '>' <|> char '~')) <|> space) <* whiteSpace
+--        simpleSelectorSequence = try ((++) <$> (try cssTypeSelector <|> cssUniversal) <*> (try selSequence <|> pure "")) <|> selSequence -- todo selSequence should be array
+--        selSequence = cssHash <|> cssClass <|> cssAttrib <|> cssPseudo <|> cssNegation
+--cssTypeSelector = cssNamespacePrefixed cssElementName
+--cssNamespacePrefixed p = try ((++) <$> cssNamespacePrefix <*> p) <|> p
+--  where cssNamespacePrefix = (++) <$> (try cssIdentifier <|> string "*" <|> pure "") <*> string "|"
+--cssElementName = cssIdentifier
+--cssUniversal = cssNamespacePrefixed (string "*")
+--cssClass = (:) <$> char '.' <*> cssIdentifier
+--cssAttrib = between (char '[') (char ']') ((++) <$> (whiteSpace *> cssNamespacePrefixed cssIdentifier <* whiteSpace) <*> cssAttribValue)
+--  where cssAttribValue = try $ option "" ((++) <$> cssOpt <*> (cssIdentifier <|> cssString))
+--        cssOpt =  try (string "~=")
+--              <|> try (string "|=")
+--              <|> try (string "^=")
+--              <|> try (string "$=")
+--              <|> try (string "*=")
+--              <|> string "="
+--cssPseudo = (++) <$> pseudoColumn <*> (try functionalPseudo <|> cssIdentifier)
+--  where pseudoColumn = try (string "::") <|> string ":"
+--        functionalPseudo = (++) <$> (cssIdentifier <* whiteSpace) <*> between (char '(') (char ')') functionParams
+--        functionParams = (++) <$> cssExpression <*> (try (many1 space *> functionParams) <|> pure "")
 
-cssNegation = (++) <$> (try $ string ":not") <*> (whiteSpace *> between (char '(') (char ')') cssNegationArg <* whiteSpace)
-  where cssNegationArg = cssUniversal <|> cssHash <|> cssClass <|> cssAttrib <|> cssPseudo <|> cssTypeSelector
+--cssNegation = (++) <$> (try $ string ":not") <*> (whiteSpace *> between (char '(') (char ')') cssNegationArg <* whiteSpace)
+--  where cssNegationArg = cssUniversal <|> cssHash <|> cssClass <|> cssAttrib <|> cssPseudo <|> cssTypeSelector
 
-cssIdentifier = (++) <$> (string "-" <|> pure "") <*> ((:) <$> cssNameStart <*> many cssNameChar)
+cssIdentifier :: Parser CssIdentifier
+cssIdentifier = T.pack <$> ((++) <$> (string "-" <|> pure "") <*> ((:) <$> cssNameStart <*> many cssNameChar))
   where cssNameStart = char '_' <|> letter
+
+cssNameChar :: Parser Char
 cssNameChar = oneOf "-_" <|> alphaNum
-cssName = many1 cssNameChar
-cssHash = (:) <$> char '#' <*> cssName
-cssString = between (char '"') (char '"') (many stringPart)
+
+cssName :: Parser CssName
+cssName = T.pack <$> many1 cssNameChar
+
+cssHash :: Parser CssHash
+cssHash = Hash <$> ((T.pack <$> string "#") *> cssName)
+
+cssString :: Parser CssString
+cssString = (Quote . T.pack) <$> between (char '"') (char '"') (many stringPart)
   where stringPart = try (string "\\\"" *> pure '"') <|> noneOf "\""
-cssExpression = many $ noneOf "( )" -- todo this should be [ [ PLUS | '-' | DIMENSION | NUMBER | STRING | IDENT ] S* ]+
 
-newLine = try $ string "\r\n" <|> string "\n" <|> string "\r" <|> string "\f"
-whiteSpace = skipMany space
+--cssExpression :: Parser CssExpression
+--cssExpression = many $ noneOf "( )" -- todo this should be [ [ PLUS | '-' | DIMENSION | NUMBER | STRING | IDENT ] S* ]+
+
+--newLine :: Parser Text
+--newLine = T.pack <$> (try $ string "\r\n" <|> string "\n" <|> string "\r" <|> string "\f")
+
+--whiteSpace :: Parser ()
+--whiteSpace = skipMany space
 
 
-parseCSS :: String -> Either ParseError String
-parseCSS input = parse cssSelectorGroup "culo" input
+--parseCSS :: Text -> Either ParseError CssSelectorsGroup
+--parseCSS input = parse cssSelectorGroup "culo" input
 
 --main =
 --    do c <- getContents
